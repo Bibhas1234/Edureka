98b8173c2bac463e350cb5fea99d694ba1b7bfc4
Bibhas1234 <154235176+Bibhas1234@users.noreply.github.com>
2023-12-22 07:35:02 +0000

    last commit

--- python.py
+++ python.py
@@ -1,2 +1,31 @@
 
-print ("hello world")
+import requests
+import logging
+
+def extract_endpoints(swagger_url):
+    logging.info(f"Fetching Swagger JSON from {swagger_url}")
+    try:
+        response = requests.get(swagger_url)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
+        swagger_json = response.json()
+
+        endpoints = []
+        for path in swagger_json.get("paths", {}):
+            endpoints.append(path)
+
+        logging.info(f"Extracted endpoints: {endpoints}")
+        return endpoints
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching Swagger JSON: {e}")
+        return None
+
+if __name__ == "__main__":
+    swagger_url = "http://petstore.swagger.io"  # Replace with your Swagger JSON URL
+    endpoints = extract_endpoints(swagger_url)
+
+    if endpoints:
+        print("List of endpoints:")
+        for endpoint in endpoints:
+            print(endpoint)
+    else:
+        print("Failed to extract endpoints.")
